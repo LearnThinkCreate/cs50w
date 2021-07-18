0ca6aacb4c7c782e310ed48555c999b0828f4374
WP <warren.hyson5@gmail.com>
2021-07-18 19:58:40 -0400

    Added index.html

--- lecture2/newyear/views.py
+++ lecture2/newyear/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    now = datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
