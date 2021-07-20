4df384784e908031bc38cf804753c126521a0350
WP <warren.hyson5@gmail.com>
2021-07-20 17:39:45 -0400

    Remove priority from task form

--- lecture2/tasks/views.py
+++ lecture2/tasks/views.py
@@ -3,7 +3,6 @@ from django import forms
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=8)
 
 tasks = ['foo', 'bar', 'baz']
 # Create your views here.
