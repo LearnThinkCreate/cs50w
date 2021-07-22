b0b46e1d77e6ed0a7392cf739cd911a1b3cbe332
WP <warren.hyson5@gmail.com>
2021-07-21 20:03:29 -0400

    Added search functionality

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
-
+from django.urls import reverse
+from django import forms
+from django.http import HttpResponseRedirect
 from . import util
 
+class search(forms.Form):
+    q = forms.CharField(max_length=100,
+    widget=forms.TextInput(attrs={'class':'search', 
+    "placeholder":"Search Encyclopedia"}), label="")
 
 def index(request):
+    if request.method == "POST":
+        # Getting the data from the form
+        form = search(request.POST)
+        if form.is_valid():
+            # Getting the query from the user
+            q = form.cleaned_data['q']
+
+            # Appending any entry that matches the query to a list
+            results = []
+            for entry in util.list_entries():
+                if q in entry:
+                    results.append(entry)
+
+            # Returning that list to the user
+            return render(request, "encyclopedia/results.html",{
+                "results":results
+            })
+            pass
+        else:
+            return render(request, "encyclopedia/error.html",{
+                pass
+            })
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form":search()
     })
 
 def wiki(request, TITLE):
