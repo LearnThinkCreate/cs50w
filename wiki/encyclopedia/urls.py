bd530707e21ba80c0166abc0238adb91c7a7fa03
WP <warren.hyson5@gmail.com>
2021-07-21 22:30:32 -0400

    Addd create new page feature

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-     path('wiki/<str:TITLE>', views.wiki, name="wiki")
+    path('wiki/<str:TITLE>', views.wiki, name="wiki"),
+    path("newPage/", views.newPage, name="newPage")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -6,8 +6,25 @@ from . import util
 
 class search(forms.Form):
     q = forms.CharField(max_length=100,
-    widget=forms.TextInput(attrs={'class':'search', 
-    "placeholder":"Search Encyclopedia"}), label="")
+    widget=forms.TextInput(attrs={
+        'class':'search', 
+        "placeholder":"Search Encyclopedia"
+    }), label="")
+
+class createPage(forms.Form):
+    title = forms.CharField(label="",
+    required=True,    
+    widget=forms.TextInput(attrs={
+        "class":"form-control form-control-lg",
+        "placeholder":"Title"
+    }))
+
+    markdown = forms.CharField(label="",
+    required=True,
+    widget=forms.Textarea(attrs={
+        "class":"form-control form-control-lg",
+        "placeholder":"Enter your Markdown Content"
+    }))
 
 def index(request):
     if request.method == "POST":
@@ -36,9 +53,13 @@ def index(request):
                 "results":results,
                 "form":search()
             })
-            # Form is not valid
-        else:
-            return render(request, "encyclopedia/error.html")
+
+        # Form is not valid
+        return render(request, "encyclopedia/error.html", {
+            "form":search()
+        })
+    
+    # Get Request
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
         "form":search()
@@ -49,5 +70,28 @@ def wiki(request, TITLE):
         "TITLE":TITLE.capitalize(),
         "content":util.get_entry(TITLE.lower()),
         "form":search()
+    })
+
+def newPage(request):
+    # Checking the request method
+    if request.method == "POST":
+        form = createPage(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["markdown"]
+
+            util.save_entry(title, content)
+            return render(request, "encyclopedia/wiki.html",{
+                    "TITLE":title,
+                    "content":content,
+                    "form":search()
+                })
+        # There form was not valid
+        return render(request, "encyclopedia/error.html")
+    
+    # GET request
+    return render(request, "encyclopedia/newPage.html", {
+        "form":search(),
+        "newPage":createPage()
     })
 
