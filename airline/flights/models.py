ad678c9399d3d98fe9a8a4a38e5604b8c8dcd3e8
WP <warren.hyson5@gmail.com>
2021-07-28 21:52:15 -0400

    Created 2 models for flights

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,8 +2,29 @@ from django.db import models
 
 # Create your models here.
 # Each object is a table that django will create for us 
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 class Flights(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(
+        # Foreign Key Model
+        Airport, 
+        # When a foreign key is deleted so are all rows that refrence it
+        on_delete=models.CASCADE,
+        # Name that can be used when refrences origin from the Airport model
+        related_name="departures"
+        )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="arrivals"
+    )
     duration = models.IntegerField()
-    
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
