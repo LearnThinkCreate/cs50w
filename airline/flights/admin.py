6e6f4f78773580ba0bcb2c5acb537d56b733be86
WP <warren.hyson5@gmail.com>
2021-07-31 16:30:44 -0400

    Added passengers to flights page, linked pages

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Flights, Airport
+from .models import Flights, Airport, Passengers
 
 # Register your models here.
 admin.site.register(Airport)
 admin.site.register(Flights)
+admin.site.register(Passengers)
