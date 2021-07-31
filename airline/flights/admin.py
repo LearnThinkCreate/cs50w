8cf9dd6499997becd3c355ba9a3200cfebe27f0e
WP <warren.hyson5@gmail.com>
2021-07-31 18:05:57 -0400

    Added login portal

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Flights, Airport, Passengers
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights", )
+
 admin.site.register(Airport)
-admin.site.register(Flights)
-admin.site.register(Passengers)
+admin.site.register(Flights, FlightAdmin)
+admin.site.register(Passengers, PassengerAdmin)
